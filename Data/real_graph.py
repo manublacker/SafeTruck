a0934d3e690625113e7ec56c3f50b64dc44e6490
manublacker <mblacker@itba.edu.ar>
2026-03-28 20:57:16 -0300

    Obtencion de nodos por coordenadas GPS con nearest_nodes

--- Data/real_graph.py
+++ Data/real_graph.py
@@ -2,7 +2,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import osmnx as ox
 G = ox.graph_from_place("Palermo, Buenos Aires")
-print(G.nodes(data=True))
-print(G.edges(data=True))
-ox.plot_graph(G)
-plt.show()
+Y = -34.6037
+X = -58.3816
+Z = -34.5833
+W = -58.4167
+node1 = ox.nearest_nodes(G, X, Y)
+node2 = ox.nearest_nodes(G, W, Z)
+print(node1)
+print(node2)
+#print(G.nodes(data=True))
+#print(G.edges(data=True))
+#ox.plot_graph(G)
+#plt.show()
